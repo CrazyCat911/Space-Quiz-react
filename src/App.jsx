3bed6fc5a3e90f47f24cd934d141ec387580ddcf
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2024-04-05 11:19:33 +1000

    Add Question with Enter key detection
    Question checking will be added later

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+// @ts-check
+import React from "react";
 import "./App.css";
+import questions from "./questions.json";
+
+function Question({ num }) {
+  const question = questions[num];
+
+  function checkAnswer(answer) {
+    console.warn("Check answer not implemented");
+  }
+
+  return (
+    <>
+      <h1>{question.question}</h1>
+      <input
+        type={question[num]}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            console.log("ENTER the other function");
+          }
+        }}
+      />
+      <button type="button">Submit</button>
+    </>
+  );
+}
 
 export default function Quiz() {
-  return <h1>Nothing here 😿</h1>;
+  return <Question num={0} />;
 }
