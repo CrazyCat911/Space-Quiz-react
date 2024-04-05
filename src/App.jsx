060d634bfbf2e18d9f7c3d9e6cb7db1711e9b110
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2024-04-05 13:42:44 +1000

    Add next quesiton functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,69 @@
-// @ts-check
-import React from "react";
 import "./App.css";
 import questions from "./questions.json";
+import { useState, useEffect } from "react";
 
-function Question({ num }) {
-  const question = questions[num];
+export default function Quiz() {
+  const [answer, setAnswer] = useState("");
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [question, setQuestion] = useState(questions[questionNumber]);
+  useEffect(() => {
+    return () => {
+      setQuestion(questions[questionNumber]);
+    };
+  }, [questionNumber]);
 
-  function checkAnswer(answer) {
-    console.warn("Check answer not implemented");
+  function isAnswerCorrect(answer) {
+    let correctAnswers = questions[questionNumber].answers;
+    // Checking for the contains answers
+    let isCorrect = false;
+    if (correctAnswers.contains != undefined) {
+      correctAnswers.contains.forEach((checkingAnswer) => {
+        if (answer.includes(checkingAnswer)) {
+          isCorrect = true;
+        }
+      });
+    }
+    // Checking for the exactly answers
+    if (correctAnswers.exactly != undefined) {
+      correctAnswers.exactly.forEach((checkingAnswer) => {
+        if (answer === checkingAnswer) {
+          isCorrect = true;
+        }
+      });
+    }
+    // At the end
+    return isCorrect;
+  }
+
+  function AnswerHandle(answer) {
+    if (isAnswerCorrect(answer)) {
+      setQuestionNumber(questionNumber + 1);
+    }
   }
 
   return (
     <>
       <h1>{question.question}</h1>
       <input
-        type={question[num]}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            console.log("ENTER the other function");
+        type={question.type}
+        onChange={(event) => {
+          setAnswer(event.target.value);
+        }}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            // console.log("ENTER the other function");
+            AnswerHandle(answer);
           }
         }}
       />
-      <button type="button">Submit</button>
+      <button
+        type="button"
+        onClick={() => {
+          AnswerHandle(answer);
+        }}
+      >
+        Submit
+      </button>
     </>
   );
-}
-
-export default function Quiz() {
-  return <Question num={0} />;
 }
